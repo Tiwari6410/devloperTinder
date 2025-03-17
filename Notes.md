3b5742744e838de642ddf0d782724b135608e144
demomo <your.name@domain.com>
2025-03-17 12:03:05 +0530

    explore with Authorization and token

--- Notes.md
+++ Notes.md
@@ -5,3 +5,8 @@ multiple route handlers - play with the code
 - next();
   -next funcion and errors along with res.send()
 - app.use("/route", rH, [rh2, rH3], rH3, rH4)
+- what is middleware
+- how express js basically handle request behind the scenes
+- difference between app.use and app.all
+- Write a dummy auth middleware for admin
+- write a dummy auth middleware for all user routes, except /user /login
--- src/app.js
+++ src/app.js
@@ -1,36 +1,29 @@
-console.log("arvind tiwari is a richest person in the world")
-
 const express = require("express")
-
 const app = express();
 
-
-app.use("/user", rH1, rH2, [rH3, rH4], rH5, rh6)
-
-app.get("/user", (req, res, next) => {
-    console.log("i am first controller console")
-    res.send({ firstName: "Arvind", second: "Tiwari" })
-    next()
-
-},
-    [(req, res, next) => {
-        console.log("i am second controller console")
-        res.send("response 2!!!")
+app.use("/admin", (req, res, next) => {
+    console.log("Authorization is getting checked")
+    let token = "xyz";
+    let isAuthorization = token === "xyz";
+    if (!isAuthorization) {
+        res.status(401).send("Unauthorized request")
+    } else {
         next()
-    },
-    (req, res, next) => {
-        console.log("i am Third controller console")
-        res.send("response 3!!!")
-        next()
-    }],
-
-    (req, res) => {
-        console.log("i am Fourth controller")
-        res.send("response 4!!!!")
+    }
+})
+// app.get("/user", (req, res, next) => {
+//     res.send("User Data sent")
+//     next()
+// })
+app.get("/admin/login", (req, res) => {
+    console.log("admin auth is getting checked")
+    res.send({
+        firstName: "Arvind tiwari",
+        address: "Gwalior Madhyapradesh"
     })
+})
 
 
-
-app.listen(7777, () => {
-    console.log("server is running successfully ")
+app.listen(9090, () => {
+    console.log("sucessufully Port is on the way")
 })
